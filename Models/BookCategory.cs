3b788b6fd11bdba5be56af5cfed2396942a2199c
jasarevicedis <e.jasarevic03@gmail.com>
2023-11-21 10:59:59 +0100

    Added book category model

---
+++ Models/BookCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace eLibraryApi.Models
+{
+    public class BookCategory
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public int BookId { get; set; }
+    }
+}
