29f6a74c966faf4793932f68f8392bc3e1e6fbf2
jasarevicedis <e.jasarevic03@gmail.com>
2023-11-21 10:58:26 +0100

    Added book model

---
+++ Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace eLibraryApi.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int copiesNumber { get; set; }
+    }
+}
