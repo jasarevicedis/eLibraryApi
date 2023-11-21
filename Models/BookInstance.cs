a0ee0137b77461ea83d6598139360fa6be93f713
jasarevicedis <e.jasarevic03@gmail.com>
2023-11-21 10:58:58 +0100

    Added Book Instance model

---
+++ Models/BookInstance.cs
@@ -0,0 +1,10 @@
+﻿namespace eLibraryApi.Models
+{
+    public class BookInstance
+    {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public bool isAvailable { get; set; }
+
+    }
+}
