374c08a8d5870dc5017bd19d75e0a99283856eaa
jasarevicedis <e.jasarevic03@gmail.com>
2023-11-21 10:59:29 +0100

    Added loan model

---
+++ Models/Loan.cs
@@ -0,0 +1,13 @@
+﻿namespace eLibraryApi.Models
+{
+    public class Loan
+    {
+        public int Id { get; set; }
+        public int BookInstanceId { get; set; }
+        public int StudentId { get; set; }
+        public DateTime LoanDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+
+    }
+}
