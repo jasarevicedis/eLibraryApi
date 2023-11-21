530d5d1dcaeea22a6a5a1a5679042ef024355719
jasarevicedis <e.jasarevic03@gmail.com>
2023-11-21 10:59:49 +0100

    Added book author model

---
+++ Models/BookAuthor.cs
@@ -0,0 +1,10 @@
+﻿namespace eLibraryApi.Models
+{
+    public class BookAuthor
+    {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public int AuthorId { get; set; }
+
+    }
+}
