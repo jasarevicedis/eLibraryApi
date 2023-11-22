981ae8df97254dc98f6237c9fdbfc5b318e274af
jasarevicedis <e.jasarevic03@gmail.com>
2023-11-22 10:53:06 +0100

    added first migration

---
+++ Data/DataContext.cs
@@ -0,0 +1,35 @@
+﻿using eLibraryApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace eLibraryApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Author> authors { get; set; }
+        public DbSet<Book> books { get; set; }
+        public DbSet<BookAuthor> books_authors { get; set; }
+        public DbSet<Category> categories { get; set; }
+        public DbSet<Loan> loans { get; set; }
+        public DbSet<Student> students { get; set; }
+        public DbSet<BookCategory> books_categories { get; set; }
+        public DbSet<BookInstance> bookinstances { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().ToTable(nameof(Book));
+            modelBuilder.Entity<Author>().ToTable(nameof(Author));
+            modelBuilder.Entity<BookAuthor>().ToTable(nameof(BookAuthor));
+            modelBuilder.Entity<Category>().ToTable(nameof(Category));
+            modelBuilder.Entity<Loan>().ToTable(nameof(Loan));
+            modelBuilder.Entity<Student>().ToTable(nameof(Student));
+            modelBuilder.Entity<BookCategory>().ToTable(nameof(BookCategory));
+            modelBuilder.Entity<BookInstance>().ToTable(nameof(BookInstance));
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+        
+}
