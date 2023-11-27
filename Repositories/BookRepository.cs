1237f4bba0d6b6bee28f844218cccbc2ad00db5d
jasarevicedis <e.jasarevic03@gmail.com>
2023-11-27 21:33:34 +0100

    updated book repo

--- Repositories/BookRepository.cs
+++ Repositories/BookRepository.cs
@@ -1,6 +1,22 @@
-﻿namespace eLibraryApi.Repository
+﻿using eLibraryApi.Data;
+using eLibraryApi.Interfaces;
+using eLibraryApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eLibraryApi.Repository
 {
-    public class BookRepository
+    public class BookRepository: IBookRepository
     {
+        private readonly DataContext _context;
+        
+        public BookRepository(DataContext context) {
+            _context = context;
+        }
+        public async Task<Book> GetBooks()
+        {
+            return await _context.books.ToListAsync();
+        }
+
+        
     }
 }
