9ae7c4ae1bd879885ec12b0df731e8e05f60ef35
jasarevicedis <e.jasarevic03@gmail.com>
2023-11-29 11:33:53 +0100

    Updated book repository

--- Repositories/BookRepository.cs
+++ Repositories/BookRepository.cs
@@ -12,11 +12,50 @@ namespace eLibraryApi.Repository
         public BookRepository(DataContext context) {
             _context = context;
         }
-        public async Task<Book> GetBooks()
+        public async Task<List<Book>> GetBooks()
         {
-            return await _context.books.ToListAsync();
+            return await _context.books
+                .Include(x=>x.Title)
+                .Include(x=>x.Description)
+                .Include(x=>x.copiesNumber)
+                .ToListAsync();
         }
 
-        
+        public async Task<bool> AddBook(Book book)
+        {
+            await _context.AddAsync(book);
+            return await Save();
+        }
+
+        public async Task<List<Book>> FilterAndOrderBooks(Category category, string SortOption)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Book> GetBookById(int id)
+        {
+            return await _context.books
+                .Where(a => a.Id == id)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Book>> GetBooksByName(string search)
+        {
+            return await _context.books
+                .Where(a => a.Title.ToLower().Contains(search.ToLower()))
+                .ToListAsync();
+        }
+
+        public async Task<bool> Save()
+        {
+            var saved = await _context.SaveChangesAsync();
+            return saved > 0;
+        }
+
+        public Task<bool> UpdateBook(Book book)
+        {
+            _context.Update(book);
+            return Save();
+        }
     }
 }
