06365a9f661b5acfb7b1036ed0bb128b22699ad5
jasarevicedis <e.jasarevic03@gmail.com>
2023-11-29 11:33:28 +0100

    Updated book controller

---
+++ Controllers/BookController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using eLibraryApi.Dto;
+using eLibraryApi.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eLibraryApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : Controller
+    {
+        private readonly IBookRepository _bookRepository;
+        private readonly IMapper _mapper;
+
+        public BookController(IBookRepository bookRepository, IMapper mapper)
+        {
+            _bookRepository = bookRepository;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(List<BookDto>))]
+        public async Task<ActionResult<List<BookDto>>> GetBooks()
+        {
+            var bookDtos = _mapper.Map<List<BookDto>>(await _bookRepository.GetBooks());
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(bookDtos);
+        }
+    }
+}
