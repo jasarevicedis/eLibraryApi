23d34542064caca11e8622827811e18e635f6981
jasarevicedis <e.jasarevic03@gmail.com>
2023-11-29 12:00:37 +0100

    updated book interface

--- Interfaces/IBookRepository.cs
+++ Interfaces/IBookRepository.cs
@@ -8,6 +8,7 @@ namespace eLibraryApi.Interfaces
         public Task<Book> GetBookById(int id);
         public Task<List<Book>> GetBooksByName(string name);
         public Task<List<Book>> FilterAndOrderBooks(Category category, string SortOption);
+        public Task<bool> BookExists(int id);
         public Task<bool> AddBook(Book book);
         public Task<bool> UpdateBook(Book book);
         public Task<bool> Save();
