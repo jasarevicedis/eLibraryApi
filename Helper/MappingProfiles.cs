d89d6b0094c142dbae1e2dae42881f2db7eeb5ae
jasarevicedis <e.jasarevic03@gmail.com>
2023-11-29 11:45:34 +0100

    added automapper, updated program class

---
+++ Helper/MappingProfiles.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using eLibraryApi.Dto;
+using eLibraryApi.Models;
+
+namespace eLibraryApi.Helper
+{
+    public class MappingProfiles: Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Book, BookDto>();
+            CreateMap<BookDto, Book>();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,33 @@
 using eLibraryApi.Data;
+using AutoMapper;
+using eLibraryApi.Helper;
+using eLibraryApi.Interfaces;
+using eLibraryApi.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddAutoMapper(typeof(MappingProfiles));
+
+builder.Services.AddScoped<IBookRepository, BookRepository>();
+
+
 
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
